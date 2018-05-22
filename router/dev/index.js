9feae6815b3ca4c5b3c284f0f7ad162361eea0d8
CZW <a19575091@gmail.com>
2018-05-22 08:24:58 +0800

    add comment history router

--- router/dev/index.js
+++ router/dev/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const post = require('./post');
 const userPostHistory = require('./userPostHistory');
+const userCommentHistory = require('./userCommentHistory');
 const comment = require('./comment');
 
 router.use(post);
 router.use(userPostHistory);
+router.use(userCommentHistory);
 router.use(comment);
 
 module.exports = router;
--- router/dev/userCommentHistory.js
+++ router/dev/userCommentHistory.js
@@ -1,5 +1,27 @@
-const router = require('express').Router()
-// todo implement
+const router = require('express').Router();
+const postData = require('./postsData.js').comments;
 
+router.get('/api/data/history/comments', (req, res, next) => {
+    const targetPosts = postData.filter(item => {
+        return item.user === req.query.user;
+    });
+    res.send(mapToCommonUserView(targetPosts));
+});
+
+function mapToCommonUserView(comments) {
+    if(comments === undefined) {
+        return {};
+    }
+
+    return comments.map(comment => {
+        return {
+            comment_id: comment.comment_id,
+            post_id: comment.post_id,
+            content: comment.content,
+            created_time: comment.created_time,
+            sequence_number: comment.sequence_number
+        }
+    })
+}
 
 module.exports = router;
