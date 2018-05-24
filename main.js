9e66df29205d3059662146f0386ec4dc96780148
CZW <a19575091@gmail.com>
2018-05-24 22:24:11 +0800

    change to https

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const https = require('https');
 const bodyParser = require('body-parser');
 const router = require('./router');
 const jwt = require('jsonwebtoken');
 const PORT = 8000;
+
+const options = {
+    key: fs.readFileSync('./privatekey.pem'),
+    cert: fs.readFileSync('./certificate.pem')
+};
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(router);
 
-app.listen(PORT, () => {
+https.createServer(options, app).listen(8000, () => {
     console.log(`env: ${process.env.NODE_ENV}`);
     console.log(`server start ${PORT}`);
-});
+})
